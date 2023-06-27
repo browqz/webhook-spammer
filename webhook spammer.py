a8161229599a0b41165ba5be76d3ce911e860099
Browqz <92259633+browqz@users.noreply.github.com>
2023-06-27 20:50:10 +0200

    Update webhook spammer.py

--- webhook spammer.py
+++ webhook spammer.py
@@ -1,12 +1,12 @@
 import requests
 
 webhook_url = 'TON_WEBHOOK'
-message_count = 10  
+nombre_message = 10  
 
-message_content = 'TON_MESSAGE'
+contenu_message = 'TON_MESSAGE'
 
-for _ in range(message_count):
-    payload = {'content': message_content}
+for _ in range(nombre_message):
+    payload = {'content': contenu_message}
     response = requests.post(webhook_url, json=payload)
     if response.status_code == 204:
         print('Message envoyé !')
